f584449fcb34cf39411de78529b3930ffcc7491c
Rajes <rajeshrao7700@gmail.com>
2025-07-10 09:10:18 +0530

    This feature branch about

--- components/About.jsx
+++ components/About.jsx
@@ -8,6 +8,7 @@ function About() {
       <h1>ABOUT</h1>
       <h2>Please find below details</h2>
       <h1>Hello All Just making changes to commit in github</h1>
+      <h3>So here I have created a feature branch, basically this change will not be seen in main letsee</h3>
       <div className='flex'>
       <User name={'Rajesh Rao (function)'}/>
       <UserClass name={'Rajesh Rao (Class)'}/>
